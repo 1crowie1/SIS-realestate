17457507ffe0823e902773eba234a52f748e1df1
JayCod3r <84632172+JayCod3r@users.noreply.github.com>
2022-09-07 22:17:57 +1000

    Some Structure to frontend
    some structure to frontend

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,22 +3,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div>
-      <div>
+
+    <div className="div-1">
+        <h1>HomeFinder</h1>
+        <div className="div-2">
         <h1>here Landing Page</h1>
-        
-      </div>
-      </div>
+        </div>
+
+
+        <div className="div-2">
         <h1>Recommendations</h1>
         <h1>Himanshu code here</h1>
+        <button className="blue-Button">Get Recommendation </button>
+        </div>
 
 
-      <div>
+        <div className="div-2">
         <h1>Results</h1>
         <h1>Amana Results page</h1>
+        </div>
+              
       
-      </div> 
+       
+      
     </div>
   );
 }
