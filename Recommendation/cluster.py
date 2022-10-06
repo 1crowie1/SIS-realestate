f0cabdfc88a5ec56e048c98356d9e343746817f6
Harrison Crowe-Maxwell <39574251+1crowie1@users.noreply.github.com>
2022-10-06 13:24:23 +1100

    Working Kmeans++ algorithm implemented with the listings data

--- Recommendation/cluster.py
+++ Recommendation/cluster.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
-import sklearn
+import pyodbc
+import matplotlib.pyplot as plt
 import logging
 
 class log_style():
@@ -15,7 +16,146 @@ class log_style():
     UNDERLINE = '\033[4m'
     RESET = '\033[0m'
 
+def log(message: str, color: str) -> None:
+    level = logging.DEBUG
+    script_name = __file__.split('\\')[-1]
+    fmt = f'[%(levelname)s] {log_style.YELLOW}{script_name}{log_style.RESET} %(asctime)s: {color}%(message)s{log_style.RESET}'
+    logging.basicConfig(level=level, format=fmt)
+    logging.info(message)
+
+class azure_db():
+    def __init__(self) -> None:
+        var_list = ['server', 'database', 'username', 'password']
+        self.credentials = pd.read_csv('Recommendation/azure_credentials.csv', names=var_list)
+        self.driver = '{SQL Server}'
+    
+    def post_azure_query(self, query: str) -> list:
+        with pyodbc.connect('DRIVER='+self.driver+';SERVER=tcp:'+self.credentials['server'][0]+';PORT=1433;DATABASE='+self.credentials['database'][0]+';UID='+self.credentials['username'][0]+';PWD='+self.credentials['password'][0]) as self.conn:
+            with self.conn.cursor() as self.cursor:
+                self.cursor.execute(query)
+                results = self.cursor.fetchall()
+        return results
+
+def check_connection() -> bool:
+    """
+    Check connection to Azure Database
+    """
+    try:
+        d = azure_db()
+        query = "SELECT @@Version"
+        results = d.post_azure_query(query)
+        log(results[0][0], log_style.GREEN)
+        return True
+    except Exception as e:
+        log(e, log_style.RED)
+        return False
+
+def get_listings() -> pd.DataFrame:
+    """
+    Get Listings from Azure Database
+    """
+    d = azure_db()
+    query = "SELECT id, property_type, price, bedrooms, bathrooms, parking_spaces FROM dbo.big_property WHERE price IS NOT NULL AND bedrooms IS NOT NULL AND bathrooms IS NOT NULL AND parking_spaces IS NOT NULL AND price < 10000000;"
+    results = d.post_azure_query(query)
+    listings = pd.DataFrame(columns = ["id", "property_type", "price", "bedrooms", "bathrooms", "parking_spaces"])
+    for row in results:
+        listings = listings.append({"id": row[0], "property_type": row[1], "price": row[2], "bedrooms": row[3], "bathrooms": row[4], "parking_spaces": row[5]}, ignore_index=True)
+    return listings
+
+class Kmeans(object):
+
+    def __init__(self, k=1):
+        self.k = k
+
+    def train(self, data, verbose=1):
+        shape = data.shape
+
+        ranges = np.zeros((shape[1], 2))
+        centroids = np.zeros((shape[1], 2))
+
+        for dim in range(shape[1]):
+            ranges[dim, 0] = np.min(data[:,dim])
+            ranges[dim, 1] = np.max(data[:,dim])
+
+        if verbose == 1:
+            print('Ranges: ')
+            print(ranges)
+
+        centroids = np.zeros((self.k, shape[1]))
+        for i in range(self.k):
+            for dim in range(shape[1]):
+                centroids[i, dim] = np.random.uniform(ranges[dim, 0], ranges[dim, 1], 1)
+
+        if verbose == 1:
+            print('Centroids: ')
+            print(centroids)
+
+            plt.scatter(data[:,0], data[:,1])
+            plt.scatter(centroids[:,0], centroids[:,1], c = 'r')
+            plt.show()
+
+        count = 0
+        while count < 100:
+            count += 1
+            if verbose == 1:
+                print('-----------------------------------------------')
+                print('Iteration: ', count)
+
+            distances = np.zeros((shape[0],self.k))
+            for ix, i in enumerate(data):
+                for ic, c in enumerate(centroids):
+                    distances[ix, ic] = np.sqrt(np.sum((i-c)**2))
+
+            labels = np.argmin(distances, axis = 1)
+
+            new_centroids = np.zeros((self.k, shape[1]))
+            for centroid in range(self.k):
+                temp = data[labels == centroid]
+                if len(temp) == 0:
+                    return 0
+                for dim in range(shape[1]): 
+                    new_centroids[centroid, dim] = np.mean(temp[:,dim])
+
+            if verbose == 1:
+                plt.scatter(data[:,0], data[:,1], c = labels)
+                plt.scatter(new_centroids[:,0], new_centroids[:,1], c = 'r')
+                plt.show()
+
+            if np.linalg.norm(new_centroids - centroids) < np.finfo(float).eps:
+                log("DONE!", "GREEN")
+                break
+
+            centroids = new_centroids
+        self.centroids = centroids
+        self.labels = labels
+        if verbose == 1:
+            print(labels)
+            print(centroids)
+        return 1
+
+    def getAverageDistance(self, data):
+
+        dists = np.zeros((len(self.centroids),))
+        for ix, centroid in enumerate(self.centroids):
+            temp = data[self.labels == ix]
+            dist = 0
+            for i in temp:
+                dist += np.linalg.norm(i - centroid)
+            dists[ix] = dist/len(temp)
+        return dists
+
+    def getLabels(self):
+        return self.labels
+
 def start():
-    logging.info(f'{log_style.MAGENTA}Creating Data-Frame{log_style.RESET}')
-    # Create a data-frame with that attributes wanted for the suburb level model
-    attributes = pd.DataFrame(columns=['suburb', 'avg_price', 'dist_hospital', 'dist_cbd', 'num_recreation'])
+    if not check_connection():
+        return
+    log("Requesting Listings", "WHITE")
+    listings = get_listings()
+    print(listings)
+    listings_data = listings.copy(deep=True)
+    listings_data = listings_data.drop(columns=['id', 'property_type', 'bathrooms', 'parking_spaces'])
+    data = listings_data.to_numpy()
+    kmeans = Kmeans(5)
+    kmeans.train(data)
+    return
