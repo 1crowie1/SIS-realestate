915de68ca96f3de625eb5ffa7946cd49ba16d251
Maria Jamal <maria.jamal@safetyculture.io>
2022-10-19 19:59:15 +1100

    added get popular suburbs

--- backend/routes/suburb.js
+++ backend/routes/suburb.js
@@ -6,6 +6,7 @@ const config = require('../config/db');
 
 const connection = new Connection(config);
 var suburbs = [];
+var popularSuburbs = [];
 
 // Attempt to connect and execute queries if connection goes through
 connection.on("connect", err => {
@@ -14,6 +15,7 @@ connection.on("connect", err => {
   } else {
     console.log("Connection Established")
     getAllSuburbs();
+    getPopularSuburbs();
   }
 });
 
@@ -48,13 +50,42 @@ function getAllSuburbs(){
     connection.execSql(request);
 }
 
+function getPopularSuburbs(){
+
+    console.log('Query DB: Get Popular Suburbs')
+
+    const request = new Request(
+      `SELECT suburb, count(*) AS count FROM [dbo].[suburb] GROUP BY suburb order by count desc;`,
+      (err, rowCount) => {
+          if (err) {
+            console.error(err.message);
+          } else {
+            console.log(`${rowCount} row(s) returned`);
+          }
+    });
+
+    request.on("row", columns => {
+          var suburb = {};
+          columns.forEach((column) => {
+            if (column.value === null) {
+                console.log('NULL');
+            } else {
+                suburb[column.metadata.colName] = column.value;
+            }
+        });
+        popularSuburbs.push(entry);
+    });
+    
+    connection.execSql(request);
+}
+
 // Get all suburbs
 router.get('/', function(req, res, next) {
     res.send(suburbs); // Return all suburbs
 });
 
 router.get('/getPopularSuburbs', function(req,res,next){
-    res.send(suburbs); // Return all suburbs
+    res.send(popularSuburbs); // Return popular suburbs
 })
 
 module.exports = router;
