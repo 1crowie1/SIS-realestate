de9f3059a693f5b51bcc4e63b249004554ef88ba
Maria Jamal <maria.jamal@safetyculture.io>
2022-10-19 23:35:59 +1100

    finished the feeling lucky endpoint

--- backend/routes/recommendation.js
+++ backend/routes/recommendation.js
@@ -19,9 +19,51 @@ connection.connect();
 
 router.get('/getRecommendedSuburbs/', (req, res) => {
     // TO DO: Calculate cluster number based on request params
-    
     var clusterNumber = 1;  // Temp placeholder for cluster number, pls replace this. second heatmap
+    getRecommendedSuburbs(clusterNumber, res);
+  });
+  
+  
+router.get('/feelingLucky/', (req, res) => {
+  
+    var maxClusterNumber = 1
+    // based on max cluster number
+    const request = new Request(
+        `SELECT MAX([cluster_num])  
+        FROM [dbo].[big_property]`,
+        (err, rowCount) => {
+            if (err) {
+                console.error(err.message);
+              } else if(rowCount !== 1){
+                console.log(`More than one result returned for max value`);
+              }else{
+                console.log("Max cluster number found");
+              }
+    });
 
+    console.log(request);
+
+    request.on("row", columns => {
+        columns.forEach((column) => {
+          if (column.value === null) {
+              console.log('NULL');
+          } else {
+              maxClusterNumber = column.value;
+              console.log("max",maxClusterNumber)
+          }
+      });
+    });
+
+    connection.execSql(request);
+
+    var clusterNumber = Math.floor(Math.random() * (maxClusterNumber - 1 + 1) + 1)
+    console.log("clusternum", clusterNumber);
+    request.on('requestCompleted', function () {
+        getRecommendedSuburbs(clusterNumber, res); // Return recommended suburbs
+    });
+  });
+
+function getRecommendedSuburbs(clusterNumber, res){
     console.log('Query DB: Get Recommended Suburbs')
 
     const request = new Request(
@@ -31,7 +73,7 @@ router.get('/getRecommendedSuburbs/', (req, res) => {
       GROUP BY SUBSTRING([suburb], 0, 25) 
       ORDER BY suburb_listing_count desc;`,
       (err, rowCount) => {
-          if (err) {
+        if (err) {
             console.error(err.message);
           } else {
             console.log(`${rowCount} row(s) returned`);
@@ -57,16 +99,6 @@ router.get('/getRecommendedSuburbs/', (req, res) => {
     request.on('requestCompleted', function () {
         res.send(recommendedSuburbs); // Return recommended suburbs
     });
-  });
-  
-  
-// router.get('/feelingLucky/', (req, res) => {
-//     // Mock data
-  
-//     // same as recommended suburb with a random cluster number
-//     // based on max cluster number
-//     // The below data will be based of random calculated properties so we will make a random selection of recommendations
-//     res.send({ body: properties });
-//   });
+}
   
 module.exports = router;
