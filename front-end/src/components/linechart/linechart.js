097c3ca8cce22d2bf911342d802a56a164f102e8
JayCod3r <84632172+JayCod3r@users.noreply.github.com>
2022-10-20 16:28:20 +1100

    linechart made
    Line-chart added

---
+++ front-end/src/components/linechart/linechart.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { Line } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+ import './linechart.css';
+
+// Chart JS Register
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+// CSS
+
+
+class LineChart  extends Component { 
+    constructor(props) {
+        super(props);
+        this.state = {
+            chartData: {
+                labels: ['1', '2', '3', '4', '5', '6'],
+                datasets: [
+                    {
+                        label: 'Average Price',
+                        data: [
+                            100,
+                            200,
+                            300,
+                            400,
+                            500,
+                            600,
+                            
+                        ],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.6)'
+                        ]
+                    }
+                ]
+            }
+        }
+    }
+
+    static defaultProps = {
+        displayTitle: true,
+        displayLegend: true,
+        legendPosition: 'right'
+    }
+
+    render() {
+        return (
+            <div className='chart'>
+                <Line 
+                    data={this.state.chartData}
+                    options={{
+                        title: {
+                            display: this.props.displayTitle,
+                            text: 'Average Price Per Month',
+                            fontSize: 25
+                        },
+                        legend: {
+                            display: this.props.displayLegend,
+                            position: this.props.legendPosition
+                        }
+                    }}
+                />
+            </div>
+        )
+    }
+}
+
+export default LineChart;
