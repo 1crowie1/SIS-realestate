38de5faa0c579c8b6ee53e4260845489a1d70cb6
Amana287 <amana.ramzeen@gmail.com>
2022-10-27 00:33:12 +1100

    broken graph
    changes to footer, suburb breakdown and price breakdown

--- front-end/src/components/results/priceBreakdown.js
+++ front-end/src/components/results/priceBreakdown.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import { Button, Container, Row, Col, Card } from "react-bootstrap";
 
+
+
 function SuburbBreakdown(listing_suburbs){
     return (
         <Row>
@@ -15,6 +17,125 @@ function SuburbBreakdown(listing_suburbs){
                                 <Card.Body>
                                     <Card.Text>Number of Lists: {list.listings_count}</Card.Text>
                                     <Card.Text>Details: {list.details}</Card.Text>
+                                    // star rating
+
+
+
+<Typography component="legend">Controlled</Typography>
+<Rating
+  name="simple-controlled"
+  value={value}
+  onChange={(event, newValue) => {
+    setValue(newValue);
+  }}
+/>
+<Typography component="legend">Read only</Typography>
+<Rating name="read-only" value={value} readOnly />
+<Typography component="legend">Disabled</Typography>
+<Rating name="disabled" value={value} disabled />
+<Typography component="legend">No rating given</Typography>
+<Rating name="no-value" value={null} />
+
+
+
+
+
+
+
+
+// graph 
+class ApexChart extends React.Component {
+        constructor(props) {
+          super(props);
+
+          this.state = {
+          
+            series: [{
+              name: 'XYZ MOTORS',
+              data: dates
+            }],
+            options: {
+              chart: {
+                type: 'area',
+                stacked: false,
+                height: 350,
+                zoom: {
+                  type: 'x',
+                  enabled: true,
+                  autoScaleYaxis: true
+                },
+                toolbar: {
+                  autoSelected: 'zoom'
+                }
+              },
+              dataLabels: {
+                enabled: false
+              },
+              markers: {
+                size: 0,
+              },
+              title: {
+                text: 'Suburb Name',
+                align: 'left'
+              },
+              fill: {
+                type: 'gradient',
+                gradient: {
+                  shadeIntensity: 1,
+                  inverseColors: false,
+                  opacityFrom: 0.5,
+                  opacityTo: 0,
+                  stops: [0, 90, 100]
+                },
+              },
+              yaxis: {
+                labels: {
+                  formatter: function (val) {
+                    return (val / 1000000).toFixed(0);
+                  },
+                },
+                title: {
+                  text: 'Price'
+                },
+              },
+              xaxis: {
+                type: 'datetime',
+              },
+              tooltip: {
+                shared: false,
+                y: {
+                  formatter: function (val) {
+                    return (val / 1000000).toFixed(0)
+                  }
+                }
+              }
+            },
+          
+          
+          };
+        }
+
+      
+
+        render() {
+          return (
+            
+
+      <div id="chart">
+  <ReactApexChart options={this.state.options} series={this.state.series} type="area" height={350} />
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
                                 </Card.Body>
                                 <Button variant="primary">Look at RealEstate</Button>
                             </Card.Body>
--- front-end/src/components/results/suburbBreakdown.js
+++ front-end/src/components/results/suburbBreakdown.js
@@ -2,6 +2,10 @@
 import React from 'react';
 import { Button, Container, Row, Col, Card } from "react-bootstrap";
 
+// Imports from doughnut graph 
+
+
+
 function SuburbBreakdown(listing_suburbs){
     return (
         <Row>
@@ -10,7 +14,99 @@ function SuburbBreakdown(listing_suburbs){
                     <Card.Title>Suburb Breakdown</Card.Title>
                 </Card.Body>
                 <Card.Body>
-                    {/* THIS SECTION IS LEFT TO DISPLAY THE PIE CHART CONTAINING PERCENTAGE BREAKDOWN OF NUMBER OF LISTINGS IN SUBURBS */}
+                    {/* THIS SECTION IS LEFT TO DISPLAY THE PIE CHART CONTAINING PERCENTAGE BREAKDOWN OF NUMBER OF LISTINGS IN SUBURBS */
+                    
+                    class App extends Component {
+                        render() {
+                            const options = {
+                                animationEnabled: true,
+                                title: {
+                                    text: "Suburb Breakdown"
+                                },
+                                subtitles: [{
+                                    text: "71% Positive",
+                                    verticalAlign: "center",
+                                    fontSize: 24,
+                                    dockInsidePlotArea: true
+                                }],
+                                data: [{
+                                    type: "doughnut",
+                                    showInLegend: true,
+                                    indexLabel: "{name}: {y}",
+                                    yValueFormatString: "#,###'%'",
+                                    dataPoints: [
+                                        { name: "Suburb 1", y: 5 },
+                                        { name: "Suburb 2", y: 31 },
+                                        { name: "Suburb 3", y: 40 },
+                                        { name: "Suburb 4", y: 17 },
+                                        { name: "Suburb 5", y: 7 }
+                                    ]
+                                }]
+                            }
+                            return (
+                            <div>
+                                <CanvasJSChart options = {options}
+                                    /* onRef={ref => this.chart = ref} */
+                                />
+                                {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+                            </div>
+                            );
+                        }
+                    }
+                    module.exports = App; 
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    }
+                    class App extends Component {
+	render() {
+		const options = {
+			animationEnabled: true,
+			title: {
+				text: "Customer Satisfaction"
+			},
+			subtitles: [{
+				text: "71% Positive",
+				verticalAlign: "center",
+				fontSize: 24,
+				dockInsidePlotArea: true
+			}],
+			data: [{
+				type: "doughnut",
+				showInLegend: true,
+				indexLabel: "{name}: {y}",
+				yValueFormatString: "#,###'%'",
+				dataPoints: [
+					{ name: "Unsatisfied", y: 5 },
+					{ name: "Very Unsatisfied", y: 31 },
+					{ name: "Very Satisfied", y: 40 },
+					{ name: "Satisfied", y: 17 },
+					{ name: "Neutral", y: 7 }
+				]
+			}]
+		}
+		return (
+		<div>
+			<CanvasJSChart options = {options}
+				/* onRef={ref => this.chart = ref} */
+			/>
+			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+		</div>
+		);
+	}
+}
+module.exports = App;
+
+
                 </Card.Body>
                 <Card.Body>
                     {
@@ -29,4 +125,8 @@ function SuburbBreakdown(listing_suburbs){
     )
 }
 
+                             
+
+
 export default SuburbBreakdown;
+
