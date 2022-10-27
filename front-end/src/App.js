5f75eeb0b7568a0dec6ea925672c48ab573d959f
Anesu Chakaingesu <anemac@Anesus-MacBook-Pro.local>
2022-10-27 20:42:28 +1100

    Start development on secondary heatmap. make primary heatmap dynamic

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,3 +1,6 @@
+// Import React
+import React, { useState, useEffect } from 'react';
+
 // Import Bootstrap
 import './App.css';
 
@@ -7,13 +10,36 @@ import Heatmap from './components/heatmap/heatmap';
 import Recommendations from './components/recommendations/recommendations';
 import Footer from './components/footer';
 
+// Import Util
+import RealestateUtil from "./util/RealestateUtil";
+const realestateUtil = new RealestateUtil();
+
 function App() {
+  const [heatmapResults, setHeatmapResults] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  realestateUtil.getGeoJSON().then((response) => {
+    setHeatmapResults(response.data);
+    setLoading(false);
+  });
+
   return (
     <div className='App'>
       <Header />
 
       {/* Top HeatMap Section */}
-      <Heatmap />
+      {loading ? (
+        <div className='loading'>
+          <div className='spinner-border' role='status'>
+            <span className='sr-only'>Loading...</span>
+          </div>
+        </div>
+        ) : heatmapResults && 
+          ( 
+            <Heatmap results={heatmapResults} />
+          )
+        }
+      
 
       <hr style={{border: "1px solid black", width: "90%", margin: "5px auto",}}></hr>
 
